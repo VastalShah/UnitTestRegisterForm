d31d8d908d5becfdb8e4f64129de529581528d48
Vatsal Shah <vashah@teksystems.com>
2021-04-26 15:12:56 +0530

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ae15da7-2b97-4af1-bb7a-d16afdd92aca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestForAngularApp.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumProject
+{
+    [TestClass]
+    public class UnitTestForAngularApp
+    {
+        [TestMethod]
+        public void RegistrationWorks()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                bool isFormValid;
+                driver.Navigate().GoToUrl("http://localhost:4200/Signup");
+
+                var name = driver.FindElementById("username");
+                Assert.IsNotNull(name);
+                name.SendKeys("Vatsal");
+
+                var email = driver.FindElementById("email");
+                Assert.IsNotNull(email);
+                email.SendKeys("vatsal@gmail.com");
+
+                var password = driver.FindElementById("password");
+                Assert.IsNotNull(password);
+                password.SendKeys("1234567");
+
+                var confirmPassword = driver.FindElementById("confirmPassword");
+                Assert.IsNotNull(confirmPassword);
+                confirmPassword.SendKeys("1234567");
+
+                var formsubmitbtn = driver.FindElementById("formsubmit");
+                Assert.IsNotNull(formsubmitbtn);
+                formsubmitbtn.Click();
+
+
+                try
+                {
+                    var expectedText = "You have succesfully registered";
+                    var alert = driver.SwitchTo().Alert();
+                    Assert.AreEqual(expectedText, alert.Text);
+                    alert.Accept();
+                    isFormValid = true;
+                }
+                catch (AssertFailedException)
+                {
+                    isFormValid = false;
+                    Console.WriteLine("Form is not submitted");
+                }
+
+                if (isFormValid)
+                {
+                    var homeLink = driver.FindElementById("home");
+                    homeLink.Click();
+                }
+            }
+        }
+    }
+}
